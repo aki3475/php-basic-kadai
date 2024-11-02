59c0f46bef6f487e1515d2a72d7c68520a962a58
aki3475 <aki10313475@gmail.com>
2024-11-02 17:14:30 +0900

    課題

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 連想配列に値を代入する
+         $vege_array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+  
+         // 改行する
+         echo '<br>';
+ 
+         // 連想配列の値を出力する
+         print_r($vege_array);
+ 
+         
+         ?>
+     </p>
+ </body>
+ 
+ </html>
